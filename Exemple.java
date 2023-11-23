7e4507a646c5f8d56d50b7277af21544c50df947
asma <117948441+12344567891011@users.noreply.github.com>
2023-11-23 09:31:44 +0000

    java

---
+++ Exemple.java
@@ -0,0 +1,6 @@
+public class Exemple {
+    public static void main(String[] args) {
+        System.out.println("hello  "+args[0]+"  !");
+        System.out.println("asmae "+args[1]+" wow");
+    }
+}
